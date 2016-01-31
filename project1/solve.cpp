db6c32ed778b2da6b3b2f89794dd0ec01ac4df09
Joshua Bradt <jbradt26@gmail.com>
2016-01-30 19:01:15 -0500

    Found solutions. Made plots.

--- project1/solve.cpp
+++ project1/solve.cpp
@@ -1,35 +1,46 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 #include <vector>
 #include <functional>
 #include <cmath>
+#include <string>
+#include <chrono>
 
 double sourceFunction(const double x)
 {
     return 100 * std::exp(-10 * x);
 }
 
-double secondDerivative(const std::function<double(double)>& f, const double x, const double stepSize)
+double exactResult(const double x)
 {
-    return (f(x+stepSize) - f(x-stepSize) - 2*f(x)) / (stepSize*stepSize);
+    return 1 - (1 - exp(-10)) * x - exp(-10 * x);
 }
 
-std::vector<double> solveEquation(const std::function<double(double)>& f, const unsigned numPts)
+inline double findStepSize(const double lb, const double ub, const unsigned long numPts)
+{
+    return (ub - lb) / (numPts + 1);
+}
+
+std::vector<double> solveEquation(const std::function<double(double)>& f, const unsigned long numPts)
 {
     const double a = -1;
     const double c = -1;
-    const double stepSize = 1 / (numPts+1);
+    const double stepSize = findStepSize(0, 1, numPts);
     std::vector<double> bs (numPts + 2, 2);
     std::vector<double> ws (numPts + 2, 0);
     for (size_t i = 1; i < numPts; i++) {
         ws.at(i) = f(i*stepSize) * stepSize * stepSize;
     }
 
+    // Forward substitution
     for (size_t i = 1; i < numPts; i++) {
         double factor = a / bs.at(i-1);
         bs.at(i) -= factor * c;
         ws.at(i) -= factor * ws.at(i-1);
     }
 
+    // Backward substitution
     for (size_t i = numPts - 1; i > 0; i--) {
         ws.at(i) -= c / bs.at(i+1) * ws.at(i+1);
     }
@@ -39,16 +50,41 @@ std::vector<double> solveEquation(const std::function<double(double)>& f, const 
 
 int main(const int argc, const char** argv)
 {
-    if (argc != 2) {
+    namespace chr = std::chrono;
+
+    if (argc != 3) {
         std::cerr << "Wrong number of args." << std::endl;
         return 1;
     }
 
-    unsigned numPts = std::atoi(argv[1]);
+    unsigned long numPts = std::stoul(argv[1]);
+    std::string filepath = argv[2];
 
-    auto result = solveEquation(sourceFunction, numPts);
-    for (const auto& v : result) {
-        std::cout << v << std::endl;
+    std::ofstream file (filepath, std::ios::out);
+    if (!file.good()) {
+        std::cerr << "File was not good" << std::endl;
+        return 1;
+    }
+
+    auto begin = chr::high_resolution_clock::now();
+    std::vector<double> result = solveEquation(sourceFunction, numPts);
+    auto end = chr::high_resolution_clock::now();
+    std::cout << "Solved in ";
+    std::cout << chr::duration_cast<chr::microseconds>(end - begin).count();
+    std::cout << " µs" << std::endl;
+
+    const double stepSize = findStepSize(0, 1, numPts);
+
+    file << std::setprecision(4) << std::fixed;
+
+    file << std::setw(10) << "x";
+    file << std::setw(10) << "Result";
+    file << std::setw(10) << "Exact" << std::endl;
+
+    for (size_t i = 0; i < result.size(); i++) {
+        file << std::setw(10) << i*stepSize;
+        file << std::setw(10) << result.at(i);
+        file << std::setw(10) << exactResult(i*stepSize) << std::endl;
     }
     return 0;
 }
