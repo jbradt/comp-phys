ccb9069a1992abceea068edd6033e3bd7c0930f3
Joshua Bradt <bradt@nscl.msu.edu>
2016-02-11 13:48:20 -0500

    Finished up project 1.

--- project1/src/time_arma_lu.cpp
+++ project1/src/time_arma_lu.cpp
@@ -12,11 +12,15 @@ int main(const int argc, const char** argv)
     namespace chr = std::chrono;
 
     unsigned long numPts = 1000;
-    int numIters = 10;
+    int numIters = 20;
+    int numPerIter = 10;
     double stepSize = findStepSize(0, 1, numPts);
 
     if (argc >= 2) {
         numPts = std::stoul(argv[1]);
+    }
+    if (argc >= 3) {
+        numPerIter = std::stoi(argv[2]);
     }
 
     arma::mat A (numPts, numPts, arma::fill::zeros);
@@ -37,9 +41,11 @@ int main(const int argc, const char** argv)
     for (int iter = -1; iter < numIters; iter++) {
         arma::mat L, U;
         auto begin = chr::high_resolution_clock::now();
-        arma::lu(L, U, A);
-        y = arma::solve(L, sourceVec);
-        x = arma::solve(U, y);
+        for (int subiter = 0; subiter < numPerIter; subiter++) {
+            arma::lu(L, U, A);
+            y = arma::solve(L, sourceVec);
+            x = arma::solve(U, y);
+        }
         auto end = chr::high_resolution_clock::now();
         if (iter >= 0) durations.push_back(end - begin);
     }
@@ -49,15 +55,18 @@ int main(const int argc, const char** argv)
     auto maxDur = durations.back();
     auto totalDur = std::accumulate(durations.begin(), durations.end(), chr::high_resolution_clock::duration::zero());
 
+    double minTime = chr::duration_cast<chr::nanoseconds>(minDur).count() * 1e-9 / numPerIter;
+    double maxTime = chr::duration_cast<chr::nanoseconds>(maxDur).count() * 1e-9 / numPerIter;
+    double meanTime = chr::duration_cast<chr::nanoseconds>(totalDur).count() * 1e-9 / numPerIter / numIters;
+
     std::cout << "Ran for " << numIters << " iterations with " << numPts << " points." << std::endl;
 
-    std::cout << std::setprecision(4);
+    std::cout << std::setprecision(4) << std::scientific;
     std::cout << std::setw(8) << "Best: ";
-    std::cout << std::setw(6) << chr::duration_cast<chr::microseconds>(minDur).count() << " µs\n";
+    std::cout << std::setw(6) << minTime << " s\n";
     std::cout << std::setw(8) << "Worst: ";
-    std::cout << std::setw(6) << chr::duration_cast<chr::microseconds>(maxDur).count() << " µs\n";
+    std::cout << std::setw(6) << maxTime << " s\n";
     std::cout << std::setw(8) << "Mean: ";
-    std::cout << std::setw(6) << chr::duration_cast<chr::microseconds>(totalDur).count() / numIters << " µs\n";
-
+    std::cout << std::setw(6) << meanTime << " s\n";
     return 0;
 }
--- project1/src/timing.cpp
+++ project1/src/timing.cpp
@@ -6,18 +6,28 @@
 
 #include "solver.h"
 
-int main()
+int main(const int argc, const char** argv)
 {
     namespace chr = std::chrono;
 
     unsigned long numPts = 100000;
     int numIters = 20;
+    int numPerIter = 10;
+
+    if (argc >= 2) {
+        numPts = std::stoul(argv[1]);
+    }
+    if (argc >= 3) {
+        numPerIter = std::stoi(argv[2]);
+    }
 
     std::vector<chr::high_resolution_clock::duration> durations;
 
     for (int i = -1; i < numIters; i++) {
         auto begin = chr::high_resolution_clock::now();
-        std::vector<double> result = solveEquation(sourceFunction, numPts);
+        for (int j = 0; j < numPerIter; j++) {
+            std::vector<double> result = solveEquation(sourceFunction, numPts);
+        }
         auto end = chr::high_resolution_clock::now();
         if (i >= 0) durations.push_back(end - begin);
     }
@@ -27,15 +37,19 @@ int main()
     auto maxDur = durations.back();
     auto totalDur = std::accumulate(durations.begin(), durations.end(), chr::high_resolution_clock::duration::zero());
 
+    double minTime = chr::duration_cast<chr::nanoseconds>(minDur).count() * 1e-9 / numPerIter;
+    double maxTime = chr::duration_cast<chr::nanoseconds>(maxDur).count() * 1e-9 / numPerIter;
+    double meanTime = chr::duration_cast<chr::nanoseconds>(totalDur).count() * 1e-9 / numPerIter / numIters;
+
     std::cout << "Ran for " << numIters << " iterations with " << numPts << " points." << std::endl;
 
-    std::cout << std::setprecision(4);
+    std::cout << std::setprecision(4) << std::scientific;
     std::cout << std::setw(8) << "Best: ";
-    std::cout << std::setw(6) << chr::duration_cast<chr::microseconds>(minDur).count() << " µs\n";
+    std::cout << std::setw(6) << minTime << " s\n";
     std::cout << std::setw(8) << "Worst: ";
-    std::cout << std::setw(6) << chr::duration_cast<chr::microseconds>(maxDur).count() << " µs\n";
+    std::cout << std::setw(6) << maxTime << " s\n";
     std::cout << std::setw(8) << "Mean: ";
-    std::cout << std::setw(6) << chr::duration_cast<chr::microseconds>(totalDur).count() / numIters << " µs\n";
+    std::cout << std::setw(6) << meanTime << " s\n";
 
     return 0;
 }
